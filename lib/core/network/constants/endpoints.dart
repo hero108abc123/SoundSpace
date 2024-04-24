fdc0c7a204b7ad559a7cdfe13cd4d112dbef343a
Giang Trinh <giangtrinh2003@gmail.com>
2024-04-24 11:30:50 +0700

    fix android  manifest

--- lib/core/network/constants/endpoints.dart
+++ lib/core/network/constants/endpoints.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
-
 import 'package:soundspace/core/common/secrets/app_secrets.dart';
 
 class Endpoints {
   Endpoints._();
-  static String baseUrl =
-      Platform.isAndroid ? AppSecrets.baseUrlAnd : AppSecrets.baseUrlIos;
+  static String baseUrl = AppSecrets.baseUrlIos;
   static String auth = "$baseUrl/Auth";
 }
